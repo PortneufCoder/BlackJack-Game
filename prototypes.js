2657e9dfc063d14665c2e1a488e5639523a02e6d
Victor Ajani <portneufcoder@gmail.com>
2018-11-29 21:29:06 -0500

    formatting

--- prototypes.js
+++ prototypes.js
@@ -1,11 +1,9 @@
-
 let cat = { name: "mimi", sheen: "Black", speak: () => "meooow" };
 cat.age = 4;
 
 console.log(cat.age);
 console.log(cat.speak());
 console.log(cat);
-
 
 // Creating objects using classes
 // class Dog {
@@ -16,21 +14,20 @@ console.log(cat);
 //   }
 //   }
 
-
-function Dog (name, color, breed) {
-    this.name = name;
-    this.color = color;
-    this.breed = breed;
+function Dog(name, color, breed) {
+  this.name = name;
+  this.color = color;
+  this.breed = breed;
 }
 
 Dog.prototype.age = 13;
 
-let myDog = new Dog('Scott', 'Black', 'Labrador');
-let secondDog = new Dog('Violette', 'Ash', 'Poodle');
+let myDog = new Dog("Scott", "Black", "Labrador");
+let secondDog = new Dog("Violette", "Ash", "Poodle");
 
 console.log(myDog.age);
 console.log(secondDog.age);
-console.log(myDog.hasOwnProperty('age')); // false because the age property is on the prototype and not the object instance.
+console.log(myDog.hasOwnProperty("age")); // false because the age property is on the prototype and not the object instance.
 
 // Whe we try to get a property from an object, JS will first check the object itself in the 'this' properites
 // If nothing is found there, it checks the prototype and returns that if it exists.
@@ -38,51 +35,46 @@ console.log(myDog.hasOwnProperty('age')); // false because the age property is o
 
 console.log(Dog.prototype);
 console.log(myDog._proto_);
-Dog.prototype === myDog._proto_ ? true : console.log('false');
+Dog.prototype === myDog._proto_ ? true : console.log("false");
 
-
-
-console.log(Object.getOwnPropertyDescriptor(myDog, 'name'));
-Object.defineProperty(myDog, 'color', {writable: false});
+console.log(Object.getOwnPropertyDescriptor(myDog, "name"));
+Object.defineProperty(myDog, "color", { writable: false });
 
 myDog.color = "Blue";
 
 console.log(myDog.color); // this gives an error as I prevented property rewrite on line 29
 
-let continents = { 
-    name: {country: 'Japan', tree: 'Blossoms'}, 
-    tree: {country: 'SouthAfrica', tree: 'Roobois'} 
+let continents = {
+  name: { country: "Japan", tree: "Blossoms" },
+  tree: { country: "SouthAfrica", tree: "Roobois" }
 };
 
-Object.freeze(continents.name)
-continents.name.country = 'Korea'; // this won't work as Objects.freeze will stop the edit of the object name and its attributes
+Object.freeze(continents.name);
+continents.name.country = "Korea"; // this won't work as Objects.freeze will stop the edit of the object name and its attributes
 console.log(continents.name.country);
 
-//for ... in 
+//for ... in
 
-for ( let propertyName in continents) {
-    let myValues = `propertyName ${continents[propertyName]}`;
-    console.log(myValues);
+for (let propertyName in continents) {
+  let myValues = `propertyName ${continents[propertyName]}`;
+  console.log(myValues);
 }
 
 // prototypal inheritance
 
 let myArr = ["green", "brown", "yellow"];
 
-Object.defineProperty(Array.prototype, 'last', {get: function() {
-    return this[this.length-1]; // this is what returns the last item
-}});
+Object.defineProperty(Array.prototype, "last", {
+  get: function() {
+    return this[this.length - 1]; // this is what returns the last item
+  }
+});
 
 let last = myArr.last; // this gets me "yellow"
 console.log(last);
 
-
-
-let protoFunc = function () {
-
-};
+let protoFunc = function() {};
 console.log(protoFunc.prototype);
 
-let hunter = {name: 'Marley'};
+let hunter = { name: "Marley" };
 console.log(hunter._proto_);
-
